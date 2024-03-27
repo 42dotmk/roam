972d0990fcd3caa0c7bd0384ef0b8a2ffca51f43
halicea <costa@codechem.com>
2024-03-27 02:00:27 +0100

    init db in place

--- lua/roam/init.lua
+++ lua/roam/init.lua
@@ -8,34 +8,55 @@ local strip = function(obj) return string.gsub(obj, "\"", "") end
 
 local M = {
     db = nil,
-    config = {}
+    config = {
+        roam_dir = vim.fn.expand("~/org"),
+        db_path = vim.fn.expand("~/.config/emacs/.local/cache/org-roam.db")
+    }
 }
 
+M.initialize_db = function()
+    if (M.db ~= nil) then
+        M.db:close()
+    end
+    local db = sqlite:open(M.config.db_path)
+    -- Create the nodes table if it does not exist
+    db:eval("CREATE TABLE IF NOT EXISTS nodes (id TEXT PRIMARY KEY, title TEXT, file TEXT)")
+    db:eval("CREATE INDEX IF NOT EXISTS title_index ON nodes(title)")
+    db:eval("CREATE INDEX IF NOT EXISTS file_index ON nodes(file)")
+    -- create the files table if it does not exist
+    db:eval("CREATE TABLE IF NOT EXISTS files (id TEXT PRIMARY KEY, title TEXT, file TEXT)")
+    db:eval("CREATE INDEX IF NOT EXISTS title_index ON files(title)")
+    db:eval("CREATE INDEX IF NOT EXISTS file_index ON files(file)")
+end
 --[[
   Setup the pluing with the given options
   and registers the commands
     Args:
     - opts: table: The options for the plugin
-        - dbPath: string: The path to the sqlite database
-    
+        - db_path: string: The path to the sqlite database
 
 ]]
 M.setup = function(opts)
-    M.config = opts or {}
-    if M.config.dbPath == nil then
-        M.config.dbPath = vim.fn.expand("~/.config/emacs/.local/cache/org-roam.db")
+    opts = opts or {}
+    M.config = vim.tbl_extend("force", M.config, opts)
+
+    -- check if file exists
+    if vim.fn.filereadable(M.config.db_path) == 0 then
+        print("Database file not found, creating a new one")
+        M.initialize_db()
     end
-    M.db = sqlite:open(M.config.dbPath)
+    M.db = sqlite:open(M.config.db_path)
     vim.cmd [[command! RoamSearch lua require('roam').search()]]
-    vim.cmd [[command! RoamOpenId lua require('roam').gotoIdUnderCursor()]]
+    vim.cmd [[command! RoamOpenId lua require('roam').goto_id_under_cursor()]]
 end
+
 
 --[[
     Returns all the nodes from the database as a table
     Returns: 
     - table: A table of nodes :
 ]]
-M.getDbData = function()
+M.get_db_data = function()
     local res = M.db:eval("SELECT * from nodes")
     return res
 end
@@ -47,7 +68,7 @@ M.search = function(opts)
     pickers.new(opts, {
         prompt_title = "Roam Search",
         finder = finders.new_table {
-            results = M.getDbData() ,
+            results = M.get_db_data() ,
             entry_maker = function(entry)
                 return {
                     value = entry,
@@ -78,7 +99,7 @@ end
     Returns:
     - string: The file path of the node
 ]]
-M.getById = function(id)
+M.get_by_id = function(id)
     local res = M.db:eval("SELECT * FROM 'nodes' WHERE id='\"" .. id .. "\"' LIMIT 1")
     if #res == 0 then
         return nil
@@ -92,7 +113,7 @@ end
     - id: string: The id of the node
 ]]
 M.open_id = function(id)
-    local file = M.getById(id)
+    local file = M.get_by_id(id)
     if file == nil then
         print("No file found for id: " .. id)
         return
@@ -104,7 +125,7 @@ end
 --[[
     Opens the file for the id under the cursor in a new buffer
 ]]
-M.gotoIdUnderCursor = function()
+M.goto_id_under_cursor = function()
     local str = vim.fn.expand('<cWORD>')
     local id = string.match(str, "id:([%w%-]+)")
     if id == nil then
@@ -113,6 +134,7 @@ M.gotoIdUnderCursor = function()
     end
     M.open_id(id)
 end
+
 
 M.setup()
 return M
